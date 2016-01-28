b9d04fb50fc3bb58a3c5ec9cb465dcb2f49e6760
Garrett Davidson <gdavids@purdue.edu>
2016-01-28 12:30:20 -0500

    Initial Commit

---
+++ HiLingual/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HiLingual
+//
+//  Created by Garrett Davidson on 1/28/16.
+//  Copyright © 2016 Team3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
