c87e7e0dcca0ca9af73ca1c1746330cc653af9db
Garrett Davidson <gdavids@purdue.edu>
2016-02-16 15:56:34 -0500

    Added and set up Facebook SDKs

---
+++ iOS-App/HiLingual/HiLingual-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  HiLingual-Bridging-Header.h
+//  HiLingual
+//
+//  Created by Garrett Davidson on 2/16/16.
+//  Copyright © 2016 Team3. All rights reserved.
+//
+
+#ifndef HiLingual_Bridging_Header_h
+#define HiLingual_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+
+#endif /* HiLingual_Bridging_Header_h */
