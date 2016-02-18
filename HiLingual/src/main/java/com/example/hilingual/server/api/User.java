ad18b05e32e79194f835e6db276c21ab5be18f33
Vincent Zhang <vzhang@purdue.edu>
2016-02-18 17:23:47 -0500

    Annotate User for Jackson JSON processing

--- HiLingual/src/main/java/com/example/hilingual/server/api/User.java
+++ HiLingual/src/main/java/com/example/hilingual/server/api/User.java
@@ -3,6 +3,9 @@ package com.example.hilingual.server.api;
  * Created by joseph on 2/18/16.
  */
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.net.URL;
 import java.util.Date;
 import java.util.Locale;
@@ -37,110 +40,137 @@ public class User {
         this.usersChattedWith = usersChattedWith;
     }
 
+    @JsonProperty
     public long getUuid() {
         return uuid;
     }
 
+    @JsonProperty
     public String getName() {
         return name;
     }
 
+    @JsonProperty
     public String getDisplayName() {
         return displayName;
     }
 
+    @JsonProperty
     public String getBio() {
         return bio;
     }
 
+    @JsonProperty
     public Gender getGender() {
         return gender;
     }
 
+    @JsonProperty
     public Date getBirthdate() {
         return birthdate;
     }
 
+    @JsonProperty
     public URL getImageURL() {
         return imageURL;
     }
 
+    @JsonProperty
     public Set<Locale> getKnownLanguages() {
         return knownLanguages;
     }
 
+    @JsonProperty
     public Set<Locale> getLearningLanguages() {
         return learningLanguages;
     }
 
+    @JsonProperty
     public Set<User> getBlockedUsers() {
         return blockedUsers;
     }
 
+    @JsonProperty
     public Set<User> getUsersChattedWith() {
         return usersChattedWith;
     }
 
+    @JsonProperty
     public void setBirthdate(Date birthdate) {
         this.birthdate = birthdate;
     }
 
+    @JsonProperty
     public void setUuid(long uuid) {
         this.uuid = uuid;
     }
 
+    @JsonProperty
     public void setName(String name) {
         this.name = name;
     }
 
+    @JsonProperty
     public void setDisplayName(String displayName) {
         this.displayName = displayName;
     }
 
+    @JsonProperty
     public void setBio(String bio) {
         this.bio = bio;
     }
 
+    @JsonProperty
     public void setGender(Gender gender) {
         this.gender = gender;
     }
 
+    @JsonIgnore
     public void addKnownLanguage(Locale language) {
         knownLanguages.add(language);
     }
 
+    @JsonIgnore
     public void removeKnownLanguage(Locale language) {
         knownLanguages.remove(language);
     }
 
+    @JsonIgnore
     public void addLearningLanguage(Locale language) {
         learningLanguages.add(language);
     }
 
+    @JsonIgnore
     public void removeLearningLanguage(Locale language) {
         learningLanguages.remove(language);
     }
 
+    @JsonIgnore
     public void addBlockedUser(User user) {
         blockedUsers.add(user);
     }
 
+    @JsonIgnore
     public void removeBlockedUser(User user) {
         blockedUsers.remove(user);
     }
 
+    @JsonIgnore
     public void addusersChattedWith(User user) {
         usersChattedWith.add(user);
     }
 
+    @JsonIgnore
     public void removeusersChattedWith(User user) {
         usersChattedWith.remove(user);
     }
 
+    @JsonIgnore
     public boolean knowsLanguage(Locale locale) {
         return knownLanguages.contains(locale);
     }
 
+    @JsonIgnore
     public boolean wantsToLearnLanguage(Locale locale) {
         return learningLanguages.contains(locale);
     }
