cc3f60e6848692251fe7fd0ded0c941c323fa202
Vincent Zhang <vzhang@purdue.edu>
2016-02-18 13:55:03 -0500

    Add database config to server config class

--- HiLingual/src/main/java/com/example/hilingual/server/config/ServerConfig.java
+++ HiLingual/src/main/java/com/example/hilingual/server/config/ServerConfig.java
@@ -9,11 +9,55 @@
 
 package com.example.hilingual.server.config;
 
+import com.bendb.dropwizard.redis.JedisFactory;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public class ServerConfig extends Configuration {
 
-    //  Nothing right now
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
 
+    @NotEmpty
+    private String sqlDbType;
 
+    @Valid
+    @NotNull
+    private JedisFactory redis = new JedisFactory();
+
+    @JsonProperty("sqlDb")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
+    @JsonProperty("sqlDb")
+    public void setDataSourceFactory(DataSourceFactory factory) {
+        this.database = factory;
+    }
+
+    @JsonProperty("sqlDbType")
+    public String getSqlDbType() {
+        return sqlDbType;
+    }
+
+    @JsonProperty("sqlDbType")
+    public void setSqlDbType(String sqlDbType) {
+        this.sqlDbType = sqlDbType;
+    }
+
+    @JsonProperty("redis")
+    public JedisFactory getRedisFactory() {
+        return redis;
+    }
+
+    @JsonProperty("redis")
+    public void setRedisFactory(JedisFactory factory) {
+        this.redis = factory;
+    }
 }
