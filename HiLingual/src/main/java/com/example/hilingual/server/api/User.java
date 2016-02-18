9086df1c82ca28104cb74aa058dcaca53d36215d
Joseph S Savastano <joseph@pal-nat186-110-217.itap.purdue.edu>
2016-02-18 15:40:59 -0500

    Added User java class.

--- HiLingual/src/main/java/com/example/hilingual/server/api/User.java
+++ HiLingual/src/main/java/com/example/hilingual/server/api/User.java
@@ -1,18 +1,88 @@
-/*
- * User.java
- * HiLingual - HiLingual
- *
- * Created by Vincent Zhang on 2/18/2016
- *
- * Copyright © 2016 Team3. All rights reserved.
- */
-
-package com.example.hilingual.server.api;
-
 /**
- * User representation
- *
- * TODO
+ * Created by joseph on 2/18/16.
  */
+import java.util.Date;
+import java.util.ArrayList;
+import java.net.URL;
+
 public class User {
+    private String UUID;
+    private String name;
+    private String displayName;
+    private String bio;
+    private Gender gender;
+    private Date birthdate;
+    private URL imageURL;
+    private ArrayList<Language> knownlanguages;
+    private ArrayList<Language> learningLanguages;
+    private ArrayList<User> blockedUsers;
+    private ArrayList<User> usersChattedWith;
+
+    public User(String UUID, String name, String displayName, String bio, Gender gender, Date birthdate,
+                URL imageURL, ArrayList<Language> knownlanguages, ArrayList<Language> learningLanguages,
+                ArrayList<User> blockedUsers, ArrayList<User> usersChattedWith) {
+        this.UUID = UUID;
+        this.name = name;
+        this.displayName = displayName;
+        this.bio = bio;
+        this.gender = gender;
+        this.birthdate = birthdate;
+        this.imageURL = imageURL;
+        this.knownlanguages = knownlanguages;
+        this.learningLanguages = learningLanguages;
+        this.blockedUsers = blockedUsers;
+        this.usersChattedWith = usersChattedWith;
+    }
+
+    public String getUUID() { return UUID; }
+    public String getName() { return name; }
+    public String getDisplayName() { return displayName; }
+    public String getBio() { return bio; }
+    public Gender getGender() { return gender; }
+    public Date getBirthdate() { return birthdate; }
+    public URL getImageURL() { return imageURL; }
+    public ArrayList<Language> getKnownlanguages() { return knownlanguages; }
+    public ArrayList<Language> getLearningLanguages() { return learningLanguages; }
+    public ArrayList<User> getBlockedUsers() { return blockedUsers; }
+    public ArrayList<User> getUsersChattedWith() { return usersChattedWith; }
+
+    public void setBirthdate(Date birthdate) { this.birthdate = birthdate; }
+    public void setUUID(String UUID) { this.UUID = UUID; }
+    public void setName(String name) { this.name = name; }
+    public void setDisplayName(String displayName) { this.displayName = displayName; }
+    public void setBio(String bio) { this.bio = bio; }
+    public void setGender(Gender gender) { this.gender = gender; }
+
+    public void addKnownLanguage(Language language) {
+        knownlanguages.add(language);
+    }
+
+    public void removeKnownLanguage(Language language) {
+        knownlanguages.remove(knownlanguages.indexOf(language));
+    }
+
+    public void addLearningLanguage(Language language) {
+        learningLanguages.add(language);
+    }
+
+    public void removeLearningLanguage(Language language) {
+        learningLanguages.remove(learningLanguages.indexOf(language));
+    }
+
+    public void addBlockedUser(User user) {
+        blockedUsers.add(user);
+    }
+
+    public void removeBlockedUser(User user) {
+        blockedUsers.remove(blockedUsers.indexOf(user));
+    }
+
+    public void addusersChattedWith(User user) {
+        usersChattedWith.add(user);
+    }
+
+    public void removeusersChattedWith(User user) {
+        usersChattedWith.remove(usersChattedWith.indexOf(user));
+    }
 }
+
