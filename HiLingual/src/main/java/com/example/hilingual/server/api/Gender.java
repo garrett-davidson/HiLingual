4383e59807f4628ac34ad2167a4e99cc284ecc7f
Joseph S Savastano <joseph@pal-nat186-110-217.itap.purdue.edu>
2016-02-18 15:48:12 -0500

    Implemented User.java

---
+++ HiLingual/src/main/java/com/example/hilingual/server/api/Gender.java
@@ -0,0 +1,6 @@
+/**
+ * Created by joseph on 2/18/16.
+ */
+public enum Gender {
+    MALE, FEMALE, NOT_SET
+}
---
+++ HiLingual/src/main/java/com/example/hilingual/server/api/Language.java
@@ -0,0 +1,7 @@
+/**
+ * Created by joseph on 2/18/16.
+ */
+//Complete list of languages will be added later.
+public enum Language {
+    ARABIC, ENGLISH
+}
