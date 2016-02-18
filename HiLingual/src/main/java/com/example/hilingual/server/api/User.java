1fd68a316aab9b0beafa750fa987620df0a629e2
Vincent Zhang <vzhang@purdue.edu>
2016-02-18 17:13:41 -0500

    Format User

--- HiLingual/src/main/java/com/example/hilingual/server/api/User.java
+++ HiLingual/src/main/java/com/example/hilingual/server/api/User.java
@@ -1,9 +1,10 @@
 /**
  * Created by joseph on 2/18/16.
  */
-import java.util.Date;
-import java.util.ArrayList;
+
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class User {
     private String UUID;
@@ -34,24 +35,73 @@ public class User {
         this.usersChattedWith = usersChattedWith;
     }
 
-    public String getUUID() { return UUID; }
-    public String getName() { return name; }
-    public String getDisplayName() { return displayName; }
-    public String getBio() { return bio; }
-    public Gender getGender() { return gender; }
-    public Date getBirthdate() { return birthdate; }
-    public URL getImageURL() { return imageURL; }
-    public ArrayList<Language> getKnownlanguages() { return knownlanguages; }
-    public ArrayList<Language> getLearningLanguages() { return learningLanguages; }
-    public ArrayList<User> getBlockedUsers() { return blockedUsers; }
-    public ArrayList<User> getUsersChattedWith() { return usersChattedWith; }
+    public String getUUID() {
+        return UUID;
+    }
 
-    public void setBirthdate(Date birthdate) { this.birthdate = birthdate; }
-    public void setUUID(String UUID) { this.UUID = UUID; }
-    public void setName(String name) { this.name = name; }
-    public void setDisplayName(String displayName) { this.displayName = displayName; }
-    public void setBio(String bio) { this.bio = bio; }
-    public void setGender(Gender gender) { this.gender = gender; }
+    public String getName() {
+        return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public URL getImageURL() {
+        return imageURL;
+    }
+
+    public ArrayList<Language> getKnownlanguages() {
+        return knownlanguages;
+    }
+
+    public ArrayList<Language> getLearningLanguages() {
+        return learningLanguages;
+    }
+
+    public ArrayList<User> getBlockedUsers() {
+        return blockedUsers;
+    }
+
+    public ArrayList<User> getUsersChattedWith() {
+        return usersChattedWith;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 
     public void addKnownLanguage(Language language) {
         knownlanguages.add(language);
