3b9b49150329fd077a7ad2a44b3b6a5ffd9f1807
Vincent Zhang <vzhang@purdue.edu>
2016-02-18 13:44:13 -0500

    Stub User representation

---
+++ HiLingual/src/main/java/com/example/hilingual/server/api/User.java
@@ -0,0 +1,18 @@
+/*
+ * User.java
+ * HiLingual - HiLingual
+ *
+ * Created by Vincent Zhang on 2/18/2016
+ *
+ * Copyright © 2016 Team3. All rights reserved.
+ */
+
+package com.example.hilingual.server.api;
+
+/**
+ * User representation
+ *
+ * TODO
+ */
+public class User {
+}
