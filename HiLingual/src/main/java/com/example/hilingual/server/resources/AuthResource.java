f0544906f9b08255e77bcbec2c12ba948f74add9
Vincent Zhang <vzhang@purdue.edu>
2016-02-22 14:48:22 -0500

    Implement auth endpoint

--- HiLingual/src/main/java/com/example/hilingual/server/resources/AuthResource.java
+++ HiLingual/src/main/java/com/example/hilingual/server/resources/AuthResource.java
@@ -9,8 +9,12 @@
 
 package com.example.hilingual.server.resources;
 
-import com.example.hilingual.server.api.AuthenticatedUser;
 import com.example.hilingual.server.api.AuthenticationRequest;
+import com.example.hilingual.server.api.AuthenticationResponse;
+import com.example.hilingual.server.dao.FacebookIntegrationDAO;
+import com.example.hilingual.server.dao.GoogleIntegrationDAO;
+import com.example.hilingual.server.dao.SessionDAO;
+import com.google.inject.Inject;
 
 import javax.validation.Valid;
 import javax.ws.rs.*;
@@ -23,26 +27,63 @@ import javax.ws.rs.core.Response;
  * <b>Endpoint base path:</b> /auth
  * <br/>
  * <b>Endpoints:</b>
- *
  */
 @Path("/auth")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AuthResource {
 
+    private final SessionDAO sessionDAO;
+    private final FacebookIntegrationDAO facebookIntegrationDAO;
+    private final GoogleIntegrationDAO googleIntegrationDAO;
+
+    @Inject
+    public AuthResource(SessionDAO sessionDAO,
+                        FacebookIntegrationDAO facebookIntegrationDAO,
+                        GoogleIntegrationDAO googleIntegrationDAO) {
+        this.sessionDAO = sessionDAO;
+        this.facebookIntegrationDAO = facebookIntegrationDAO;
+        this.googleIntegrationDAO = googleIntegrationDAO;
+    }
+
     @POST
     @Path("login")
-    public AuthenticatedUser logIn(@Valid AuthenticationRequest body) {
-
-        throw new ServerErrorException(Response.Status.NOT_IMPLEMENTED);
+    public AuthenticationResponse logIn(@Valid AuthenticationRequest body) {
+        boolean ok;
+        long userId = 0;
+        String authorityAccountId = body.getAuthorityAccountId();
+        String authorityToken = body.getAuthorityToken();
+        switch (body.getAuthority()) {
+            case FACEBOOK:
+                ok = facebookIntegrationDAO.
+                        isValidFacebookSession(authorityAccountId, authorityToken);
+                if (ok) {
+                    userId = facebookIntegrationDAO.getUserIdFromFacebookAccountId(authorityAccountId);
+                }
+                break;
+            case GOOGLE:
+                ok = googleIntegrationDAO.
+                        isValidGoogleSession(authorityAccountId, authorityToken);
+                if (ok) {
+                    userId = googleIntegrationDAO.getUserIdFromGoogleAccountId(authorityAccountId);
+                }
+                break;
+            default:
+                throw new BadRequestException();
+        }
+        if (!ok) {
+            throw new ClientErrorException(Response.Status.UNAUTHORIZED);
+        }
+        String sessionId = sessionDAO.newSession(userId);
+        return new AuthenticationResponse(userId, sessionId);
     }
 
 
     @POST
-    @Path("user/{user-id}/logout")
+    @Path("logout/{user-id}")
     public Response logOut(@HeaderParam("Authorization") String sessionToken,
                            @PathParam("user-id") long userId) {
-
+        sessionDAO.revokeSession(sessionToken, userId);
         return Response.noContent().build();
     }
 
