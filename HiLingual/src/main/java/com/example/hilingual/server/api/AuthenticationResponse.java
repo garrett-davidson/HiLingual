ac5b202854d88329fe13ad1fdd77647cc7c45f9f
Vincent Zhang <vzhang@purdue.edu>
2016-02-22 14:20:03 -0500

    Replace AuthenticatedUser w/ AuthenticationResponse

---
+++ HiLingual/src/main/java/com/example/hilingual/server/api/AuthenticationResponse.java
@@ -0,0 +1,39 @@
+/*
+ * AuthenticatedUser.java
+ * HiLingual - HiLingual
+ *
+ * Created by Vincent Zhang on
+ *
+ * Copyright © 2016 Team3. All rights reserved.
+ */
+
+package com.example.hilingual.server.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class AuthenticationResponse {
+
+    private long userId;
+
+    private String sessionId;
+
+    @JsonProperty
+    public long getUserId() {
+        return userId;
+    }
+
+    @JsonProperty
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    @JsonProperty
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    @JsonProperty
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+}
