80a6dadb8438299082ed35f915aa28e5f23ca8c5
Garrett Davidson <gdavids@purdue.edu>
2016-02-25 16:17:43 -0500

    Created and implemented LanguageSelectionViewController

---
+++ iOS-App/LanguageSelectionTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LanguageSelectionTableViewController.swift
+//  HiLingual
+//
+//  Created by Garrett Davidson on 2/25/16.
+//  Copyright © 2016 Team3. All rights reserved.
+//
+
+import Foundation
+
+class LanguageSelectionTableViewController: UITableViewController {
+
+    //Make sure you set this from wherever you are using this view!!!
+    var selectedLanguages: [Languages]!
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("languageCell")!
+        let currentLanguage = Languages.allValues[indexPath.row]
+        cell.textLabel!.text = currentLanguage.rawValue
+
+        if (selectedLanguages.contains(currentLanguage)) {
+            cell.selected = true
+            cell.accessoryType = .Checkmark
+        }
+        else {
+            cell.selected = false
+            cell.accessoryType = .None
+        }
+
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let selectedLanguage = Languages.allValues[indexPath.row]
+        if let languageIndex = selectedLanguages.indexOf(selectedLanguage) {
+            selectedLanguages.removeAtIndex(languageIndex)
+        }
+
+        else {
+            selectedLanguages.append(selectedLanguage)
+        }
+
+        tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Languages.allValues.count
+    }
+}
+
+protocol LanguageSelectionDelegate {
+    func setLangauges(languages: [Languages])
+}
