75a079987bdb1a8febb7d23123afc39381110731
Garrett Davidson <gdavids@purdue.edu>
2016-02-28 15:45:54 -0500

    Began implementing HLUserSession

--- iOS-App/HiLingual/HLUserSession.swift
+++ iOS-App/HiLingual/HLUserSession.swift
@@ -8,9 +8,39 @@
 
 import Foundation
 
-class HLUserSession {
+enum LoginAuthority: String {
+    case Facebook = "FACEBOOK"
+    case Google = "GOOGLE"
+}
+
+class HLUserSession: NSCoding {
+    let sessionId: String
+    let authority: LoginAuthority
+    let authorityAccountId: String
+    let authorityToken: String
+
+    init(sessionId: String, authority: LoginAuthority, authorityAccountId: String, authorityToken: String) {
+        self.sessionId = sessionId
+        self.authority = authority
+        self.authorityAccountId = authorityAccountId
+        self.authorityToken = authorityToken
+    }
+
+    @objc required init?(coder aDecoder: NSCoder) {
+        self.sessionId = aDecoder.decodeObjectForKey("sessionId") as! String
+        self.authority = LoginAuthority(rawValue:aDecoder.decodeObjectForKey("authority") as! String)!
+        self.authorityAccountId = aDecoder.decodeObjectForKey("authorityAccountId") as! String
+        self.authorityToken = aDecoder.decodeObjectForKey("authorityToken") as! String
+    }
+
+    @objc func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(sessionId, forKey: "sessionId")
+        aCoder.encodeObject(authority.rawValue, forKey: "authority")
+        aCoder.encodeObject(authorityAccountId, forKey: "authorityAccountId")
+        aCoder.encodeObject(authorityToken, forKey: "authorityToken")
+    }
+
     func isValid() -> Bool {
-        //Check to make sure the current token is valid
         return true
     }
 }
