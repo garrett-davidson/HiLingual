0088d1e2c1fe7c6ded5c679785bb8b2cb0d1c1ff
Garrett Davidson <gdavids@purdue.edu>
2016-03-03 00:03:45 -0500

    Implemented language selection for ProfileViewController

--- iOS-App/HiLingual/ProfileViewController.swift
+++ iOS-App/HiLingual/ProfileViewController.swift
@@ -12,18 +12,24 @@ import UIKit
 //Displays the current user's profile
 //Facilitates editing of any aspect of the profile
 
-class ProfileViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate
+class ProfileViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate, LanguageSelectionDelegate
 {
 
     @IBOutlet var editProfileView: EditProfileView!
     @IBOutlet var profileView: ProfileView!
+
     var user: HLUser?
+    var selectedLanguages: [Languages]?
+
+
     @IBAction func tapEdit(sender: AnyObject) {
         if(editProfileView.hidden == true){
             editProfileView.hidden = false
             profileView.hidden = true
             self.navigationItem.rightBarButtonItem?.title = "Done"
-        }else{
+        }
+
+        else {
             editProfileView.user.save()
             editProfileView.hidden = true
             profileView.hidden = false
@@ -36,9 +42,28 @@ class ProfileViewController: UIViewController,UIImagePickerControllerDelegate,UI
     @IBAction func showSettings(sender: AnyObject) {
         performSegueWithIdentifier("SettingsSegue", sender: self)
     }
+
     override func viewDidLoad() {
         editProfileView.user = HLUser.getCurrentUser()
         profileView.user = HLUser.getCurrentUser()
+        editProfileView.languageSelectionDelegate = self
     }
 
+    func setNewSelectedLanguages(selectedLanguages: [Languages]) {
+        self.editProfileView.updateSelectedLanguages(selectedLanguages)
+    }
+
+    func performLanguageSelectionSegue(selectedLanguages: [Languages]) {
+        self.selectedLanguages = selectedLanguages
+        self.performSegueWithIdentifier("SelectLanguagesSegue", sender: nil)
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let destNav = segue.destinationViewController as? UINavigationController {
+            if let dest = destNav.topViewController as? LanguageSelectionTableViewController {
+                dest.delegate = self
+                dest.selectedLanguages = self.selectedLanguages
+            }
+        }
+    }
 }
