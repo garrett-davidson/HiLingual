82c35910b83db154dce7108bc6ebb4721b841604
nohlson <nateohlson@gmail.com>
2016-03-04 01:48:24 -0500

    hilingualclient beta is finished. basic functionality

--- HiLingualAdminClient/hilingualclient.py
+++ HiLingualAdminClient/hilingualclient.py
@@ -1,71 +1,131 @@
 #interface with gethilingual.com!!!!!!
+#nateohlson 2016
 import sys
 import urllib3
 import certifi
 import json
 
-
 userSessionId = ""
 userId = ""
-
+loggedin = 0
 
 http = urllib3.PoolManager(
 	cert_reqs="CERT_REQUIRED",
 	ca_certs=certifi.where())
 
-
-
 def searchUsers():
-	pass
+	global responsebody
+	global userSessionId
+	global userId
 
-def updateUserInfo():
-	pass
+	query = input("Search: ")
+	url = 'https://gethilingual.com/api/user/search'
 
+	auth_param = "HLAT " + userSessionId
 
+	response = http.request('GET', url, {'query':query}, headers={'Authorization':auth_param})
+	if response.status != 200:
+		print("Error: returned status code: " + str(response.status))
+		return {}
+
+	responsebody = response.data.decode('utf-8')
+	parsed_search_responsebody = json.loads(responsebody)
+
+	return parsed_search_responsebody
+
+def updateUserInfo(previous):
+	global responsebody
+	global userSessionId
+	global userId
+
+	print("Fields:\n    1:user_name\n    2:display_name\n    3:bio\n    4:gender(male/female)\n    5:birth_date(epochseconds)\n    6:image_url\n")
+
+	fieldnum = input("\nEnter the field number you would like to edit: ")
+	newval = input("Enter new value: ")
+
+	if fieldnum == "1":
+		previous.pop("name")
+		previous["name"] = str(newval)
+	elif fieldnum == "2":
+		previous.pop("displayName")
+		previous["displayName"] = str(newval)
+	elif fieldnum == "3":
+		previous.pop("bio")
+		previous["bio"] = str(newval)
+	elif fieldnum == "4":
+		previous.pop("gender")
+		previous["gender"] = str(newval).upper()
+	elif fieldnum == "5":
+		previous.pop("birthdate")
+		previous["birthdate"] = str(newval)
+	elif fieldnum == "6":
+		previous.pop("imageURL")
+		previous["imageURL"] = str(newval)
+
+	url = 'https://gethilingual.com/api/user/' + str(userId)
+
+	auth_param = "HLAT " + str(userSessionId)
+	data = json.dumps(previous)
+
+	response = http.request('PATCH', url, headers={'Content-Type':'application/json', 'Authorization':auth_param}, body=data)
+
+	if response.status !=200:
+		print("FAILED TO UPDATE USER")
+		print("Error returned status code: " + str(response.status) + "\n\n")
+		return 0
+
+	print("\nSuccessful update\n\n")
 
 def printUsersInfo(parsedjson):
+	print("\nYOUR PROFILE:")
+	if "userId" in parsedjson:
+		print("    userId: " + str(parsedjson["userId"]))
+	if "name" in parsedjson:
+		print("    Name: " + parsedjson["name"])
+	if "displayName" in parsedjson:
+		print("    Display Name: " + parsedjson["displayName"])
+	if "bio" in parsedjson:
+		print("    Bio: " + parsedjson["bio"])
+	if "gender" in parsedjson:
+		print("    Gender: " + parsedjson["gender"])
+	if "birthdate" in parsedjson:
+		print("    Birthdate: " + str(parsedjson["birthdate"]))
+	if "imageURL" in parsedjson:
+		print("    imageURL: " + parsedjson["imageURL"])
+	if "knownLanguages" in parsedjson:
+		print("    Known Languages: " + str(parsedjson["knownLanguages"]))
+	if "learningLanguages" in parsedjson:
+		print("    Learning Languages: " + str(parsedjson["learningLanguages"]))
+	if "blockedUsers" in parsedjson:
+		print("    Blocked Users: " + str(parsedjson["blockedUsers"]))
+	if "usersChattedWith" in parsedjson:
+		print("    Users Chatted With: " + str(parsedjson["usersChattedWith"]))
+
+	print("\n")
+
+
+def printUsersInfoSearch(parsedjson):
 	print("Returned user information:")
+	print(parsedjson)
 
-	for user in parsedjson:
-		if "userId" in user:
-			print("    userId: " + user["userId"])
-		if "name" in user:
-			print("    Name: " + user["name"])
-		if "displayName" in user:
-			print("    Display Name: " + user["displayName"])
-		if "bio" in user:
-			print("    Bio: " + user["bio"])
-		if "gender" in user:
-			print("    Gender: " + user["gender"])
-		if "birthdate" in user:
-			print("    Birthdate: " + user["birthdate"])
-		if "imageURL" in user:
-			print("    imageURL: " + user["imageURL"])
-		if "knownLanguages" in user:
-			print("    Known Languages:")
-			for x in user["knownLanguages"]:
-				print("        " + x)
-		if "learningLanguages" in user:
-			print("    Learning Languages:")
-			for x in user["learningLanguages"]:
-				print("        " + x)
-		if "blockedUsers" in user:
-			print("    Blocked Users:")
-			for x in user["blockedUsers"]:
-				print("        " + x)
-		if "usersChattedWith" in user:
-			print("    Users Chatted With:")
-			for x in user["usersChattedWith"]:
-				print("        " + x)
+	if parsedjson:
+		print(len(parsedjson))
+		for subdict in parsedjson:
+			print("USER: " + subdict["displayName"])
+			for subkey in subdict:
+				print("    " + subkey + ": " + str(subdict[subkey]))
+			print("\n\n")
 
-
+		
+	else:
+		print("No results")
 
 def logout():
 	url = 'https://gethilingual.com/api/auth/'+ str(userId) + '/logout'
 
 	auth_param = "HLAT " + userSessionId
 
-	data = '{"userId":"' + str(userId) + '"}'
+	data = '{"UserId":"' + str(userId) + '"}'
 
 	print(auth_param)
 
@@ -79,22 +139,19 @@ def logout():
 
 	sys.exit(1)
 
-
-
-def getProfileInfo():
+def getOtherProfileInfo(thisid):
 	global responsebody
 	global userSessionId
-	global userId
 
-	url = 'https://gethilingual.com/api/user/' + userId
+	url = 'https://gethilingual.com/api/user/' + str(thisid)
 
 	auth_param = "HLAT " + userSessionId
 
-	data = '{"userId":"' + userId + '"}'
+	data = '{"thisid":"' + str(thisid) + '"}'
 
 	response = http.request('GET', url, headers={'Content-Type':'application/json', 'Authorization':auth_param}, body=data)
 	if response.status != 200:
-		print("Error: returned status code: " + response.status)
+		print("Error: returned status code: " + str(response.status))
 		return {}
 
 	responsebody = response.data.decode('utf-8')
@@ -102,32 +159,48 @@ def getProfileInfo():
 
 	return parsed_login_responsebody
 
+def getProfileInfo():
+	global responsebody
+	global userSessionId
+	global userId
 
+	url = 'https://gethilingual.com/api/user/' + str(userId)
 
+	auth_param = "HLAT " + userSessionId
+
+	data = '{"userId":"' + str(userId) + '"}'
+
+	response = http.request('GET', url, headers={'Content-Type':'application/json', 'Authorization':auth_param}, body=data)
+	if response.status != 200:
+		print("Error: returned status code: " + str(response.status))
+		return {}
+
+	responsebody = response.data.decode('utf-8')
+	parsed_login_responsebody = json.loads(responsebody)
+
+	return parsed_login_responsebody
 
 def userview():
 	print("LOGIN SUCCESS")
 	while 1:
-		print("1:Get My Profile Info\n2:Get user profile info\n3:Update profile\n4:Search users\n5:Logout")
+		print("1:Get My Profile Info\n2:Get Another user profile info\n3:Update profile\n4:Search users\n5:Logout")
 		decision = input("What would you like to do>")
 		if decision == "1":
-			getProfileInfo()
-		elif decision == "2":
 			returned = getProfileInfo()
+			printUsersInfo(returned)
+		elif decision == "2":
+			otherid = input("Enter the id number of user: ")
+			returned = getOtherProfileInfo(otherid)
 			printUsersInfo(returned)
 		elif decision == "3":
 			returned = getProfileInfo()
 			updateUserInfo(returned)
 		elif decision == "4":
 			returned = searchUsers()
-			printUsersInfo(returned)
+			printUsersInfoSearch(returned)
 
 		elif decision == "5":
 			logout()
-
-
-
-
 
 def login():
 	global http
@@ -141,16 +214,15 @@ def login():
 	authorityAccountId = input("Enter authority account id:")
 	authorityToken = input("Enter authority token:")
 
-
-
-
+	#hardcoded for testing
+	# authority = "FACEBOOK"
+	# authorityAccountId = "100844556977738"
+	# authorityToken = "CAAOcGlfuCWEBAEAyq4qdPZAisXgJ50LLcASiITW9QZCArmZCWjepDWqlZCXqfH4qpHxJFb6R3YyxdcZAlVsGZBdC7JgvTQ6RgU4EncrUWYeKgCaROXC2ImOUv7cK5fTf5JyRwuyMTHbg0nZBTeKZC7Siqs2cIDgfJgDUGnQH83gCmT5PZAZBHDoJseJTNUg1ZCZCy3dDu0ArcmhmRGonIVNxpRq3"
 
 	data = '{"authority":"'
 	data = data + authority + '","authorityAccountId":"'
 	data = data + authorityAccountId + '","authorityToken":"'
 	data = data + authorityToken + '"}'
-
-
 
 	response = http.request('POST', url, headers={'Content-Type':'application/json'}, body=data)
 	responsebody = response.data.decode("utf-8")
@@ -167,10 +239,6 @@ def login():
 	print("UserID: " + str(userId))
 	return 1
 
-
-	
-
-
 def register():
 	url = 'https://gethilingual.com/api/auth/register'
 
@@ -185,11 +253,8 @@ def register():
 
 	response = requests.get(url, data=data)
 
-
-
-
-
 def main():
+	global loggedin
 
 	while 1:
 		option = initview()
@@ -201,14 +266,12 @@ def main():
 			success = register()
 
 		if success == 1:
+			loggedin = 1
 			userview()
+
 		else:
 			print("There was an error logging in")
 			sys.exit(1)
-
-
-
-
 
 
 def initview():
@@ -219,7 +282,11 @@ def initview():
 
 
 
-
-
-
-main()
+try:
+	main()
+	# for debugging
+	# login()
+	# userview()
+except KeyboardInterrupt:
+	if loggedin == 1:
+		logout()
