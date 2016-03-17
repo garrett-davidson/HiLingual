ead206f18f5796691b42d7f6c1f6ff45c516b3e8
Garrett Davidson <gdavids@purdue.edu>
2016-03-16 22:44:06 -0400

    Created MessageTableViewCell class

---
+++ iOS-App/HiLingual/MessageTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MessageTableViewCell.swift
+//  HiLingual
+//
+//  Created by Garrett Davidson on 3/16/16.
+//  Copyright © 2016 Team3. All rights reserved.
+//
+
+import Cocoa
+
+class MessageTableViewCell: UITableViewCell {
+
+}
