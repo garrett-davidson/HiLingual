8757ff1a6ae307e2735d702738c4abdd2d7d5a65
Vincent Zhang <vzhang@purdue.edu>
2016-04-03 10:49:56 -0400

    Impl Message model

--- HiLingual/src/main/java/com/example/hilingual/server/api/Message.java
+++ HiLingual/src/main/java/com/example/hilingual/server/api/Message.java
@@ -1,6 +1,65 @@
 package com.example.hilingual.server.api;
 
-public class Message {
-    //  TODO
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class Message {
+
+    private long id;
+    private long timestamp;
+    private String content;
+    private String editData;
+
+    public Message(long id, long timestamp, String content, String editData) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.content = content;
+        this.editData = editData;
+    }
+
+    public Message(String content) {
+        this(0, 0, content, null);
+    }
+
+    public Message() {
+    }
+
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @JsonProperty
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    @JsonProperty
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @JsonProperty
+    public String getContent() {
+        return content;
+    }
+
+    @JsonProperty
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @JsonProperty
+    public String getEditData() {
+        return editData;
+    }
+
+    @JsonProperty
+    public void setEditData(String editData) {
+        this.editData = editData;
+    }
 }
