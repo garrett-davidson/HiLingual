eb693a4943b95bc1f0774c790b2f675fb882221f
Garrett Davidson <gdavids@purdue.edu>
2016-04-04 12:14:00 -0400

    Added test case for long message editing bug

--- iOS-App/HiLingualUITests/HiLingualUITests.swift
+++ iOS-App/HiLingualUITests/HiLingualUITests.swift
@@ -96,5 +96,16 @@ class HiLingualUITests: XCTestCase {
         
         
     }
+
+    func testEditingLongMessage() {
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["This is an already accepted request"].tap()
+        tablesQuery.staticTexts["Long ass message incoming HAHAHAHAAHAHAHAHAAHAHAHAHHAAHAHAHAHAHAHHAHAHAHAAHAHAHAHAAHAHAHAHHAAHAHAHAHAHAHHAHAHAHAAHAHAHAHAAHAHAHAHHAAHAHAHAHAHAHHAHAHAHAAHAHAHAHAAHAHAHAHHAAHAHAHAHAHAHHAHAHAHAAHAHAHAHAAHAHAHAHHAAHAHAHAHAHAHHAHAHAHAAHAHAHAHAAHAHAHAHHAAHAHAHAHAHAHHAHAHAHAAHAHAHAHAAHAHAHAHHAAHAHAHAHAHAHHAHAHAHAAHAHAHAHAAHAHAHAHHAAHAHAHAHAHAH"].tap()
+        app.menuItems["Edit"].tap()
+        
+        let textView = XCUIApplication().otherElements["TestView"].childrenMatchingType(.Other).element.childrenMatchingType(.TextView).element
+        XCTAssert(textView.frame.size.height > 2)
+    }
     
 }
