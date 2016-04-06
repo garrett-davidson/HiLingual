25de9264a7806bba1b9ff5fa7525686ce615669f
Vincent Zhang <vzhang@purdue.edu>
2016-04-05 22:30:43 -0400

    Fix Jedis concurrency issue

--- HiLingual/src/main/java/com/example/hilingual/server/dao/impl/DeviceTokenDAOImpl.java
+++ HiLingual/src/main/java/com/example/hilingual/server/dao/impl/DeviceTokenDAOImpl.java
@@ -3,6 +3,7 @@ package com.example.hilingual.server.dao.impl;
 import com.example.hilingual.server.dao.DeviceTokenDAO;
 import com.google.inject.Inject;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Transaction;
 
 import java.util.Set;
@@ -14,11 +15,11 @@ public class DeviceTokenDAOImpl implements DeviceTokenDAO {
 
     public static final String HL_DEVICE_TOKEN_USER_SCOPE = "hl:devicetokens:user:";
     public static final String HL_DEVICE_TOKEN_USERS_KEY = "hl:devicetokens:users";
-    private Jedis jedis;
+    private JedisPool pool;
 
     @Inject
-    public DeviceTokenDAOImpl(Jedis jedis) {
-        this.jedis = jedis;
+    public DeviceTokenDAOImpl(JedisPool pool) {
+        this.pool = pool;
     }
 
     @Override
@@ -28,46 +29,71 @@ public class DeviceTokenDAOImpl implements DeviceTokenDAO {
 
     @Override
     public void addDeviceToken(long userId, String token) {
-        String key = userIdKey(userId);
-        Transaction t = jedis.multi();
-        t.sadd(key, token);
-        t.sadd(HL_DEVICE_TOKEN_USERS_KEY, longStr(userId));
-        t.exec();
+        Jedis jedis = pool.getResource();
+        try {
+            String key = userIdKey(userId);
+            Transaction t = jedis.multi();
+            t.sadd(key, token);
+            t.sadd(HL_DEVICE_TOKEN_USERS_KEY, longStr(userId));
+            t.exec();
+        } finally {
+            pool.returnResourceObject(jedis);
+        }
     }
 
     @Override
     public void revokeUserDeviceToken(long userId, String token) {
-        String key = userIdKey(userId);
-        jedis.srem(key, token);
-        if (jedis.scard(key) == 0) {
-            revokeAllUserDeviceTokens(userId);
+        Jedis jedis = pool.getResource();
+        try {
+            String key = userIdKey(userId);
+            jedis.srem(key, token);
+            if (jedis.scard(key) == 0) {
+                revokeAllUserDeviceTokens(userId);
+            }
+        } finally {
+            pool.returnResourceObject(jedis);
         }
     }
 
     @Override
     public int revokeAllUserDeviceTokens(long userId) {
-        String key = userIdKey(userId);
-        int ret = jedis.scard(key).intValue();
-        Transaction t = jedis.multi();
-        t.del(key);
-        t.srem(HL_DEVICE_TOKEN_USERS_KEY, longStr(userId));
-        return ret;
+        Jedis jedis = pool.getResource();
+        try {
+            String key = userIdKey(userId);
+            int ret = jedis.scard(key).intValue();
+            Transaction t = jedis.multi();
+            t.del(key);
+            t.srem(HL_DEVICE_TOKEN_USERS_KEY, longStr(userId));
+            return ret;
+        } finally {
+            pool.returnResourceObject(jedis);
+        }
     }
 
     @Override
     public void truncate() {
-        Set<String> vals = jedis.smembers(HL_DEVICE_TOKEN_USERS_KEY);
-        Transaction t = jedis.multi();
-        vals.stream().
-                map(this::userIdKey).
-                forEach(t::del);
-        t.del(HL_DEVICE_TOKEN_USERS_KEY);
-        t.exec();
+        Jedis jedis = pool.getResource();
+        try {
+            Set<String> vals = jedis.smembers(HL_DEVICE_TOKEN_USERS_KEY);
+            Transaction t = jedis.multi();
+            vals.stream().
+                    map(this::userIdKey).
+                    forEach(t::del);
+            t.del(HL_DEVICE_TOKEN_USERS_KEY);
+            t.exec();
+        } finally {
+            pool.returnResourceObject(jedis);
+        }
     }
 
     @Override
     public Set<String> getUserDeviceTokens(long userId) {
-        return jedis.smembers(userIdKey(userId));
+        Jedis jedis = pool.getResource();
+        try {
+            return jedis.smembers(userIdKey(userId));
+        } finally {
+            pool.returnResourceObject(jedis);
+        }
     }
 
     @Override
--- HiLingual/src/main/java/com/example/hilingual/server/dao/impl/SessionDAOImpl.java
+++ HiLingual/src/main/java/com/example/hilingual/server/dao/impl/SessionDAOImpl.java
@@ -12,6 +12,7 @@ package com.example.hilingual.server.dao.impl;
 import com.example.hilingual.server.dao.SessionDAO;
 import com.google.inject.Inject;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Transaction;
 
 import java.math.BigInteger;
@@ -29,12 +30,12 @@ public class SessionDAOImpl implements SessionDAO {
     public static final String HL_SESSIONS_USER_SCOPE = "hl:sessions:user:";
     public static final String HL_SESSIONS_SESSION_SCOPE = "hl:sessions:session:";
     public static final String HL_SESSIONS_USERS_KEY = "hl:sessions:users";
-    private Jedis jedis;
+    private JedisPool pool;
     private Random random;
 
     @Inject
-    public SessionDAOImpl(Jedis jedis) {
-        this.jedis = jedis;
+    public SessionDAOImpl(JedisPool pool) {
+        this.pool = pool;
     }
 
 
@@ -51,68 +52,101 @@ public class SessionDAOImpl implements SessionDAO {
         if (userId == -1) {
             return false;
         }
-        return jedis.sismember(userIdKey(userId), sessionId);
+        Jedis jedis = pool.getResource();
+        try {
+            return jedis.sismember(userIdKey(userId), sessionId);
+        } finally {
+            pool.returnResourceObject(jedis);
+        }
     }
 
     @Override
     public void revokeSession(String sessionId, long userId) {
         String key = userIdKey(userId);
-        Transaction t = jedis.multi();
-        t.srem(key, sessionId);
-        t.srem(HL_SESSIONS_USERS_KEY, longStr(userId));
-        t.exec();
-        if (jedis.scard(key) == 0) {
-            revokeAllSessionsForUser(userId);
+        Jedis jedis = pool.getResource();
+        try {
+            Transaction t = jedis.multi();
+            t.srem(key, sessionId);
+            t.srem(HL_SESSIONS_USERS_KEY, longStr(userId));
+            t.exec();
+            if (jedis.scard(key) == 0) {
+                revokeAllSessionsForUser(userId);
+            }
+        } finally {
+            pool.returnResourceObject(jedis);
         }
     }
 
     @Override
     public int revokeAllSessions() {
-        Set<String> sessionedUsers = jedis.smembers(HL_SESSIONS_USERS_KEY);
-        LongAdder adder = new LongAdder();
-        sessionedUsers.stream().
-                mapToLong(Long::parseLong).
-                forEach(u -> adder.add(revokeAllSessionsForUser(u)));
-        return (int) adder.sum();
+        Jedis jedis = pool.getResource();
+        try {
+            Set<String> sessionedUsers = jedis.smembers(HL_SESSIONS_USERS_KEY);
+            LongAdder adder = new LongAdder();
+            sessionedUsers.stream().
+                    mapToLong(Long::parseLong).
+                    forEach(u -> adder.add(revokeAllSessionsForUser(u)));
+            return (int) adder.sum();
+        } finally {
+            pool.returnResourceObject(jedis);
+        }
     }
 
     @Override
     public int revokeAllSessionsForUser(long userId) {
-        String key = userIdKey(userId);
-        List<String> sessions = getAllSessionsForUser(userId);
-        Transaction t = jedis.multi();
-        t.del(key);
-        sessions.stream().
-                map(this::sessionKey).
-                forEach(t::del);
-        t.srem(HL_SESSIONS_USERS_KEY, longStr(userId));
-        t.exec();
-        return sessions.size();
+        Jedis jedis = pool.getResource();
+        try {
+            String key = userIdKey(userId);
+            List<String> sessions = getAllSessionsForUser(userId);
+            Transaction t = jedis.multi();
+            t.del(key);
+            sessions.stream().
+                    map(this::sessionKey).
+                    forEach(t::del);
+            t.srem(HL_SESSIONS_USERS_KEY, longStr(userId));
+            t.exec();
+            return sessions.size();
+        } finally {
+            pool.returnResourceObject(jedis);
+        }
     }
 
     @Override
     public String newSession(long userId) {
-        String sid = new BigInteger(130, random).toString(32);
-        String userIdStr = longStr(userId);
-        Transaction t = jedis.multi();
-        t.sadd(userIdKey(userId), sid);
-        t.set(sessionKey(sid), userIdStr);
-        t.sadd(HL_SESSIONS_USERS_KEY, userIdStr);
-        t.exec();
-        return sid;
+        Jedis jedis = pool.getResource();
+        try {
+            String sid = new BigInteger(130, random).toString(32);
+            String userIdStr = longStr(userId);
+            Transaction t = jedis.multi();
+            t.sadd(userIdKey(userId), sid);
+            t.set(sessionKey(sid), userIdStr);
+            t.sadd(HL_SESSIONS_USERS_KEY, userIdStr);
+            t.exec();
+            return sid;
+        } finally {
+            pool.returnResourceObject(jedis);
+        }
     }
 
     @Override
     public List<String> getAllSessionsForUser(long userId) {
-        return new ArrayList<>(jedis.smembers(userIdKey(userId)));
+        Jedis jedis = pool.getResource();
+        try {
+            return new ArrayList<>(jedis.smembers(userIdKey(userId)));
+        } finally {
+            pool.returnResourceObject(jedis);
+        }
     }
 
     @Override
     public long getSessionOwner(String sessionId) {
+        Jedis jedis = pool.getResource();
         try {
             return Long.parseLong(jedis.get(sessionKey(sessionId)));
         } catch (NumberFormatException e) {
             return -1;
+        } finally {
+            pool.returnResourceObject(jedis);
         }
     }
 
