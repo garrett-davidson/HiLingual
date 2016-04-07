dbda7a00c84c34a6ec5068ce475f8557c4ea5bee
Garrett Davidson <gdavids@purdue.edu>
2016-04-07 18:41:17 -0400

    Added HLServer object for abstracting server requests

---
+++ iOS-App/HiLingual/HLServer.swift
@@ -0,0 +1,166 @@
+//
+//  HLServer.swift
+//  HiLingual
+//
+//  Created by Garrett Davidson on 4/7/16.
+//  Copyright © 2016 Team3. All rights reserved.
+//
+
+import Foundation
+
+
+class HLServer {
+
+    static let apiBase = "https://gethilingual.com/api/"
+
+    static func sendRequest(request: NSURLRequest) -> NSDictionary? {
+        var resp: NSURLResponse?
+
+        if let returnedData = try? NSURLConnection.sendSynchronousRequest(request, returningResponse: &resp) {
+            if let returnString = NSString(data: returnedData, encoding: NSUTF8StringEncoding) {
+                print(returnString)
+
+                if let response = resp as? NSHTTPURLResponse {
+                    switch response.statusCode {
+                    case 200..<300:
+                        if let ret = (try? NSJSONSerialization.JSONObjectWithData(returnedData, options: NSJSONReadingOptions(rawValue: 0))) as? NSDictionary {
+                            return ret
+                        }
+
+                        else {
+                            //We succeeded but the result was empty
+                            return NSDictionary()
+                        }
+
+                    case 300..<400:
+                        print("Result redirected")
+                        print("Was that supposed to happen...? 🤔")
+
+                    case 400..<500:
+                        print("You probably fucked up the request 😓")
+
+                    case 503:
+                        print("*********Server Down********* 😭")
+
+                    case 500..<600:
+                        print("Server fucked up 😏")
+
+                    default:
+                        print("Wtf just happened??? 💩")
+                    }
+                }
+
+
+                else {
+                    print("Couldn't parse return dictionary")
+                }
+            }
+
+            else {
+                print("Response not a string")
+            }
+        }
+        else {
+            print("Served returned no data")
+        }
+
+        if resp != nil {
+            print("Server Response: ", resp!)
+        }
+
+        return nil
+    }
+
+    static func sendRequestToEndpoint(endpoint: String, method: String, withDictionary dict: Dictionary<String, AnyObject>?, authenticated: Bool=true) -> NSDictionary? {
+        let request = NSMutableURLRequest(URL: NSURL(string: apiBase + endpoint)!)
+
+        var headerFields = ["Content-Type": "application/json"]
+
+        if authenticated {
+            if let session = HLUser.getCurrentUser().getSession() {
+                headerFields["Authorization"] = "HLAT " + session.sessionId
+            }
+
+            else {
+                print("Could not authenticate")
+                return nil
+            }
+        }
+
+        request.HTTPMethod = method
+
+        if dict != nil {
+            request.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(NSDictionary(dictionary: dict!), options: NSJSONWritingOptions(rawValue: 0))
+        }
+
+
+        return sendRequest(request)
+    }
+
+    static func sendGETRequestToEndpoint(endpoint: String, withParameterString parameters: String?=nil, authenticated: Bool=true) -> NSDictionary? {
+        var urlString = apiBase + endpoint
+
+        if parameters != nil {
+            urlString += parameters!
+        }
+
+        let request = NSMutableURLRequest(URL: NSURL(string: urlString)!)
+
+        var headerFields = ["Content-Type": "application/json"]
+
+        if authenticated {
+            if let session = HLUser.getCurrentUser().getSession() {
+                headerFields["Authorization"] = "HLAT " + session.sessionId
+            }
+
+            else {
+                print("Could not authenticate")
+                return nil
+            }
+        }
+
+        request.allHTTPHeaderFields = headerFields
+
+        request.HTTPMethod = "GET"
+        
+        return sendRequest(request)
+    }
+
+    static func getTranslationForMessage(message: HLMessage, fromLanguage: String?, toLangauge: String="en-US") -> String? {
+
+        if let ret = sendGETRequestToEndpoint("chat/\(message.receiverID)/message/\(message.messageUUID!)/translate") {
+            return ret["translatedContent"] as? String
+        }
+
+        return nil
+    }
+
+    static func saveEdit(editedText: String, forMessage message: HLMessage) -> Bool {
+        if let encodedEdit = editedText.dataUsingEncoding(NSUTF8StringEncoding)?.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0)) {
+            if sendRequestToEndpoint("chat/\(message.senderID)/message/\(message.messageUUID!)", method: "PATCH", withDictionary: ["editData" : encodedEdit]) != nil {
+                return true
+            }
+        }
+
+        return false
+
+//        let request = NSMutableURLRequest(URL: NSURL(string: "https://gethilingual.com/api/chat/\(senderID)/message/\(messageUUID!)")!)
+//        if let session = HLUser.getCurrentUser().getSession() {
+//
+//            request.allHTTPHeaderFields = ["Content-Type": "application/json", "Authorization": "HLAT " + session.sessionId]
+//            request.HTTPMethod = "PATCH"
+//
+//
+//
+//            request.HTTPBody = try? NSJSONSerialization.dataWithJSONObject(NSDictionary(dictionary: ), options: NSJSONWritingOptions(rawValue: 0))
+//
+//            if let returnedData = try? NSURLConnection.sendSynchronousRequest(request, returningResponse: nil) {
+//                print(returnedData)
+//                if let returnString = NSString(data: returnedData, encoding: NSUTF8StringEncoding) {
+//                    print(returnString)
+//                    return
+//                }
+//            }
+//        }
+    }
+}
