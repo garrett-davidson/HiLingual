51d8ab92afd7b7227c889418624f60d1d2334633
nohlson <nateohlson@gmail.com>
2016-04-08 00:26:10 -0400

    register test

--- HiLingualServerTests/hilingualservertests.py
+++ HiLingualServerTests/hilingualservertests.py
@@ -1,6 +1,9 @@
 #hilingual automated tests
 #nateohlson
 
+import sys
+import json
+
 
 
 def test7():
@@ -24,17 +27,66 @@ def test3():
 def test2():
 	print("Test 2: Change Profile Info")
 
+	#
+
+
 def test1():
 	print("Test 1: Register new users")
+	url = 'https://gethilingual.com/api/auth/register'
+	data = '{"authority":"'
+	data = data + authority + '","authorityAccountId":"'
+	data = data + authorityAccountId + '","authorityToken":"'
+	data = data + authorityToken + '"}'
+
+
+	response = http.request('POST', url, headers={'Content-Type':'application/json'}, body=data)
+	responsebody = response.data.decode("utf-8")
+	if response.status != 200:
+		print("Test failed")
+		print(responsebody)
+		sys.exit(1)
+	else:
+		parsed_login_responsebody = json.loads(responsebody)
+		print(responsebody)
+		userSessionId = parsed_login_responsebody["sessionId"]
+		userId = parsed_login_responsebody["userId"]
+		print("SessionID: " + userSessionId)
+		print("UserID: " + str(userId))
+		print("Test Passed!")
+
+
 
 
 
 def main():
+	global authAthorityUser1
+	global authAthorityUser2
+	global authorityAccountIdUser1
+	global authorityAccountIdUser2
+
 	testRegistration = input("Do you want to test registration?\n1)Yes\n2)No\n>")
+
+
+	authAthorityUser1  = input("What is the name of the authorization authority for user1:\n    1)Facebook\n    2)Google\n>")
+	if authAthorityUser1 == "1":
+		authAthorityUser1 = "FACEBOOK"
+	elif authAthorityUser1 == "2":
+		authAthorityUser1 = "GOOGLE"
+
+	authAthorityUser2  = input("What is the name of the authorization authority for user2:\n    1)Facebook\n    2)Google\n>")
+	if authAthorityUser2 == "1":
+		authAthorityUser2 = "FACEBOOK"
+	elif authAthorityUser2 == "2":
+		authAthorityUser2 = "GOOGLE"
+
+	authorityAccountIdUser1 = input("Enter authority account id for user1:")
+	authorityTokenUser1 = input("Enter authority token for user1:")
+
+	authorityAccountIdUser2 = input("Enter authority account id for user2:")
+	authorityTokenUser2 = input("Enter authority token for user2:")
 
 	if testRegistration == "1":
 		test1()
-		
 
 	test2()
 
