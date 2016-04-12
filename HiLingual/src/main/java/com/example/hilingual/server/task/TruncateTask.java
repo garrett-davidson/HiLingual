6933025ba378f4a11cfd320d39b059f3ddfef4cb
Vincent Zhang <vzhang@purdue.edu>
2016-04-12 13:42:40 -0400

    Update truncate task

--- HiLingual/src/main/java/com/example/hilingual/server/task/TruncateTask.java
+++ HiLingual/src/main/java/com/example/hilingual/server/task/TruncateTask.java
@@ -9,10 +9,7 @@
 
 package com.example.hilingual.server.task;
 
-import com.example.hilingual.server.dao.FacebookIntegrationDAO;
-import com.example.hilingual.server.dao.GoogleIntegrationDAO;
-import com.example.hilingual.server.dao.SessionDAO;
-import com.example.hilingual.server.dao.UserDAO;
+import com.example.hilingual.server.dao.*;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.inject.Inject;
 import io.dropwizard.servlets.tasks.Task;
@@ -25,17 +22,23 @@ public class TruncateTask extends Task {
     private final FacebookIntegrationDAO facebookIntegrationDAO;
     private final GoogleIntegrationDAO googleIntegrationDAO;
     private final SessionDAO sessionDAO;
+    private final ChatMessageDAO chatMessageDAO;
+    private final DeviceTokenDAO deviceTokenDAO;
 
     @Inject
     public TruncateTask(UserDAO userDAO,
                         FacebookIntegrationDAO facebookIntegrationDAO,
                         GoogleIntegrationDAO googleIntegrationDAO,
-                        SessionDAO sessionDAO) {
+                        SessionDAO sessionDAO,
+                        ChatMessageDAO chatMessageDAO,
+                        DeviceTokenDAO deviceTokenDAO) {
         super("truncate-databases");
         this.userDAO = userDAO;
         this.facebookIntegrationDAO = facebookIntegrationDAO;
         this.googleIntegrationDAO = googleIntegrationDAO;
         this.sessionDAO = sessionDAO;
+        this.chatMessageDAO = chatMessageDAO;
+        this.deviceTokenDAO = deviceTokenDAO;
     }
 
     @Override
@@ -48,14 +51,18 @@ public class TruncateTask extends Task {
             output.flush();
             return;
         }
-        output.println("Truncating user database (1/4)...");
+        output.println("Truncating user database (1/6)...");
         userDAO.truncate();
-        output.println("Truncating Facebook database (2/4)...");
+        output.println("Truncating Facebook database (2/6)...");
         facebookIntegrationDAO.truncate();
-        output.println("Truncating Google database (3/4)...");
+        output.println("Truncating Google database (3/6)...");
         googleIntegrationDAO.truncate();
-        output.println("Truncating session database (4/4)...");
+        output.println("Truncating session database (4/6)...");
         sessionDAO.truncate();
+        output.println("Truncating chat database (5/6)...");
+        chatMessageDAO.truncate();
+        output.println("Truncating device token database (6/6)...");
+        deviceTokenDAO.truncate();
         output.println("Databases truncated. I hope you knew what you were doing.");
     }
 }
