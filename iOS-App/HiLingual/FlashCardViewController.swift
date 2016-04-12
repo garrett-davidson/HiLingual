7637839dd644c092ff36e207da9ea2b2d29607a4
nmaxey <nmaxey@purdue.edu>
2016-04-12 14:54:52 -0400

    made base flashcard UI

---
+++ iOS-App/HiLingual/FlashCardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FlashCardView.swift
+//  HiLingual
+//
+//  Created by Noah Maxey on 4/12/16.
+//  Copyright © 2016 Team3. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FlashCardViewController: UIViewController{
+    
+    
+}
