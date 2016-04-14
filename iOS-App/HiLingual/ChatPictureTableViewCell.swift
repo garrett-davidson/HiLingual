b8f63f85394bf6492880db100b8b7b3f823d438d
shaw65 <shaw65@purdue.edu>
2016-04-14 14:10:04 -0400

    finished initial picture sending implementation

---
+++ iOS-App/HiLingual/ChatPictureTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPictureTableViewCell.swift
+//  HiLingual
+//
+//  Created by Riley Shaw on 4/14/16.
+//  Copyright © 2016 Team3. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class ChatPictureTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var leftPicture: UIImageView!
+    @IBOutlet weak var rightPicture: UIImageView!
+}
