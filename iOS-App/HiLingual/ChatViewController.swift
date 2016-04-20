daab46b355218d757141ef06d6532f2420009bf4
shaw65 <shaw65@purdue.edu>
2016-04-20 18:36:23 -0400

    adding fullscreen images

--- iOS-App/HiLingual/ChatViewController.swift
+++ iOS-App/HiLingual/ChatViewController.swift
@@ -516,7 +516,10 @@ class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDe
         }
     }
     //For riley
-    @IBAction func handleTap(sender: AnyObject) {
+    @IBAction func handleTap(sender: UIImageView) {
+        print(sender);
+        performSegueWithIdentifier("showImage", sender: nil)
+        
         print("tap")
     }
 
@@ -524,21 +527,7 @@ class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDe
         curPlayingMessage!.setImage(UIImage(named: "shittyplay")?.imageWithRenderingMode(.AlwaysOriginal), forState: .Normal)
         isPlayingMessage = false;
     }
-    func showPicture(sender: UIImageView){
-        let deviceURL = messages[sender.tag].messageUUID!
-        let documentsURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
-        let pictureURL = documentsURL.URLByAppendingPathComponent("\(deviceURL).png")
 
-        if let filePath = NSBundle.mainBundle().pathForResource(String(deviceURL), ofType: "png"), image = UIImage(contentsOfFile: filePath) {
-            sender.contentMode = .ScaleAspectFit
-            sender.image = image
-        }else{
-              ChatViewController.loadFileSync(messages[sender.tag].pictureURL!,writeTo: pictureURL, completion:{(audioURL:String, error:NSError!) in
-                print("downloaded to: \(pictureURL)")
-            })
-            showPicture(sender)
-        }
-    }
 
     @IBAction func tapPlayButton(sender: UIButton) {
         if isPlayingMessage == true {
---
+++ iOS-App/HiLingual/PictureFullScreenViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PictureFullScreenViewController.swift
+//  HiLingual
+//
+//  Created by Riley Shaw on 4/20/16.
+//  Copyright © 2016 Team3. All rights reserved.
+//
+
+import Foundation
+
+class PictureFullScreenViewController: UIViewController {
+
+        
+}
